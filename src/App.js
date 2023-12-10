e2996d34d5c7e02ebb9ed8d6977ce01af488b97b
Vedat Yıldırım <vedatyildirim3107@gmail.com>
2023-12-10 18:52:06 +0300

    Birthday sablonu olusturuldu

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <main>
+    <section className='container'>
+      <h3>Bugun 0 dogum gunu var</h3>
+    </section>
+  </main>;
+}
+
+export default App;
